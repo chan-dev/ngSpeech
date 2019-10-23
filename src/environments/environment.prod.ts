a2a128727916d9c1c1d6862b4aac00ec17134b94
christian austria <christian.m.austria@gmail.com>
2019-10-23 14:20:52 +0800

    Setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBTT-9ahZJpZC_owgagilVgVVzOIxaEV_g',
+    authDomain: 'ngspeech-a2bb9.firebaseapp.com',
+    databaseURL: 'https://ngspeech-a2bb9.firebaseio.com',
+    projectId: 'ngspeech-a2bb9',
+    storageBucket: 'ngspeech-a2bb9.appspot.com',
+    messagingSenderId: '1055145527461',
+    appId: '1:1055145527461:web:719ee951680ede2994e54c',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBTT-9ahZJpZC_owgagilVgVVzOIxaEV_g",
+    authDomain: "ngspeech-a2bb9.firebaseapp.com",
+    databaseURL: "https://ngspeech-a2bb9.firebaseio.com",
+    projectId: "ngspeech-a2bb9",
+    storageBucket: "ngspeech-a2bb9.appspot.com",
+    messagingSenderId: "1055145527461",
+    appId: "1:1055145527461:web:719ee951680ede2994e54c"
+  }
 };
 
 /*
