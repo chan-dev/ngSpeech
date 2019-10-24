d968eb3a5cd184363ab2ebf934fd2de3c72b2386
christian austria <christian.m.austria@gmail.com>
2019-10-24 11:33:10 +0800

    Use wrapper component as the parent component for authenticated users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WrapperComponent } from './core/components/wrapper/wrapper.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/speeches',
-  },
-  {
-    path: 'speeches',
-    loadChildren: () =>
-      import('./speech/speech.module').then(m => m.SpeechModule),
+    component: WrapperComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/speeches',
+        pathMatch: 'full',
+      },
+      {
+        path: 'speeches',
+        loadChildren: () =>
+          import('./speech/speech.module').then(m => m.SpeechModule),
+      },
+    ],
   },
   {
     path: 'auth',
