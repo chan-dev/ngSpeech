8efeda11b1728714fc1e54079e3cfbe7f6ef2c0b
christian austria <christian.m.austria@gmail.com>
2019-10-28 12:57:56 +0800

    Export the imported modules in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,5 +6,6 @@ import { AngularMaterialModule } from './angular-material.module';
 @NgModule({
   declarations: [],
   imports: [CommonModule, ReactiveFormsModule, AngularMaterialModule],
+  exports: [CommonModule, ReactiveFormsModule, AngularMaterialModule],
 })
 export class SharedModule {}
